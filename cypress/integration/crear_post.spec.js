1635897954e2456350deb145dbd603ee9d69b30f
Andres Benavides <andres.benavides@eventsite.co>
2021-05-10 14:53:19 -0500

    escenarios craecion post

--- cypress/integration/crear_post.spec.js
+++ cypress/integration/crear_post.spec.js
@@ -15,15 +15,72 @@ describe('Crear post', () => {
     const tituloPost = "Ahora si que funciono esto"
     it('Crear un post 1',()=>{
     
-      crearPostPage.login("USERNAME","PASSWPRD");
+      crearPostPage.login("andre0190@gmail.com","andres1503");
       crearPostPage.clickBotonCrearPost();
       crearPostPage.infoParaPost(tituloPost);
       //crearPostPage.clickBotonVolver();
     });
 
     it('Crear un post 2',()=>{
-      crearPostPage.login("USERNAME","PASSWORD");
+      crearPostPage.login("andre0190@gmail.com","andres1503");
       crearPostPage.validarPostCreado(tituloPost);
+      cy.wait(2000)
+      
+    });
+  });
+
+  context('Crear un post y publicarlo',()=>{
+
+    const tituloPost = "Publicar este post"
+    it('Creo el post que voy a publicar',()=>{
+    
+      crearPostPage.login("andre0190@gmail.com","andres1503");
+      crearPostPage.clickBotonCrearPost();
+      crearPostPage.infoParaPost(tituloPost);
+      //crearPostPage.clickBotonVolver();
+    });
+
+    it('Publico el post que cree',()=>{
+      crearPostPage.login("andre0190@gmail.com","andres1503");
+      crearPostPage.publicarPostCreado(tituloPost);
+      cy.wait(2000)
+      
+    });
+  });
+
+  context('Publicar un post solo con acceso para miembros',()=>{
+
+    const tituloPost = "Publicar este post"
+    it('Creo el post que voy a publicar',()=>{
+    
+      crearPostPage.login("andre0190@gmail.com","andres1503");
+      crearPostPage.clickBotonCrearPost();
+      crearPostPage.infoParaPost(tituloPost);
+      //crearPostPage.clickBotonVolver();
+    });
+
+    it('Cambio el acceso y publico el post que cree',()=>{
+      crearPostPage.login("andre0190@gmail.com","andres1503");
+      crearPostPage.publicarPostAccessMembers(tituloPost);
+      cy.wait(2000)
+      
+    });
+  });
+
+  context('Ver preview del post',()=>{
+
+    const tituloPost = "Publicar este post"
+    it('Creo el post que voy a publicar',()=>{
+    
+      crearPostPage.login("andre0190@gmail.com","andres1503");
+      crearPostPage.clickBotonCrearPost();
+      crearPostPage.infoParaPost(tituloPost);
+      //crearPostPage.clickBotonVolver();
+    });
+
+    it('ver el preview del post',()=>{
+      crearPostPage.login("andre0190@gmail.com","andres1503");
+      crearPostPage.verPreviewDelPost(tituloPost);
       cy.wait(2000)
       
     });
--- cypress/page-objects/crear-post-page.js
+++ cypress/page-objects/crear-post-page.js
@@ -27,13 +27,52 @@ export class CrearPostPage{
     this.todoPage.clickBoton(".gh-editor-back-button",1000)
   }
 
-  validarPostCreado(tituloPost){
+  ultimoPostCreado(tituloPost){
     cy.get("a[href='#/posts/']").first().click();
     cy.wait(1000)
     cy.get('.gh-list-data.gh-post-list-title h3').should(($title) => {
       expect($title.first()).to.contain(tituloPost);
     });
+  }
+  
+  publicarPost(){
     
+    cy.wait(2000)
+    this.todoPage.clickBoton('.gh-publishmenu .gh-btn-editor',0)
+    this.todoPage.clickBoton('.gh-publishmenu-button',0)
+    cy.wait(2000)
+    cy.get('.gh-notification-title').should(($title) => {
+      expect($title.first()).to.contain("Published");
+      
+    });
+  }
+
+  validarPostCreado(tituloPost){
+    this.ultimoPostCreado(tituloPost);
+  }
+
+  publicarPostCreado(tituloPost){
+    this.ultimoPostCreado(tituloPost);
+    cy.get('.gh-list-data.gh-post-list-title').first().click({force: true});
+    this.publicarPost();
+  }
+
+  publicarPostAccessMembers(tituloPost){
+    this.ultimoPostCreado(tituloPost);
+    cy.get('.gh-list-data.gh-post-list-title').first().click({force: true});
+    cy.wait(1000);
+    this.todoPage.clickBoton('.gh-actions-cog',500);
+    cy.get('.gh-select select').select('members');
+    this.todoPage.clickBoton('.settings-menu-header-action',500);
+    this.publicarPost();
+  }
+
+  verPreviewDelPost(tituloPost){
+    this.ultimoPostCreado(tituloPost);
+    cy.get('.gh-list-data.gh-post-list-title').first().click({force: true});
+    cy.wait(1000);
+    this.todoPage.clickBoton('.gh-editor-preview-trigger',2000);
     
   }
+
 }
